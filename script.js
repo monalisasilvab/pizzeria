da1078cd734ac3710c4448c4921bed4f0a890b41
monalisasilvab <monalisasb91@gmail.com>
2025-12-10 23:34:38 -0300

    feat(modal): add modal close functionality with fase-out animation

--- script.js
+++ script.js
@@ -43,3 +43,15 @@ pizzaJson.map((item, index) => {
     
     c('.pizza-area').append(pizzaItem);
 });
+
+// Modal Events
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+});
