bd7e8b2ad599e7a097e45a9aea4a273cb41ae483
monalisasilvab <monalisasb91@gmail.com>
2025-12-16 14:21:12 -0300

    feat(cart): add updateCart to toggle cart visibility and iterate items

--- script.js
+++ script.js
@@ -96,5 +96,19 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
         });
     }
 
+    updateCart();
     closeModal();
 });
+
+function updateCart() {
+    if(cart.length > 0) {
+        c('aside').classList.add('show');
+
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+
+        }
+    } else {
+        c('aside').classList.remove('show');
+    }
+}
