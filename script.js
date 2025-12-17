256211f550d7e3a4ade14a049e2c0c6ec21eb7f4
monalisasilvab <monalisasb91@gmail.com>
2025-12-17 11:49:30 -0300

    feat(ui): implement cart sidebar open/close and update cart summary & count

--- script.js
+++ script.js
@@ -100,7 +100,19 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
     closeModal();
 });
 
+c('.menu-openner').addEventListener('click', () => {
+    if(cart.length > 0) {
+        c('aside').style.left = '0';
+    }
+});
+
+c('.menu-closer').addEventListener('click', () => {
+    c('aside').style.left = '100vw';
+});
+
 function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length;
+
     if(cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
@@ -158,5 +170,6 @@ function updateCart() {
 
     } else {
         c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
     }
 }
